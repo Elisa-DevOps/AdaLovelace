d9f21b169ec8d1b0d3b82db67c92b1a42ec52c2b
Elisa <elisa-devops@users.noreply.github.com>
2022-07-11 15:42:14 -0500

    added analytical engine img and js click event listeners

--- main.js
+++ main.js
@@ -11,3 +11,18 @@ let hide = function() {
 
 view.addEventListener('click', open);
 first.addEventListener('click', hide);
+
+//
+let view1 = document.getElementById('theEngine');
+let engine = document.getElementById('engineWrapper');
+
+let open1 = function() {
+    engine.style.display = 'block';
+};
+
+let close = function() {
+    engine.style.display = 'none';
+};
+
+view1.addEventListener('click', open1);
+engine.addEventListener('click', close);
