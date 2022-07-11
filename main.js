ff37e15d8bfbb6b4192111d849e21334fb36cd35
Elisa <elisa-devops@users.noreply.github.com>
2022-07-11 16:01:36 -0500

    added Ada program icon with slogan and detail message including click event listeners

--- main.js
+++ main.js
@@ -13,10 +13,10 @@ view.addEventListener('click', open);
 first.addEventListener('click', hide);
 
 //
-let view1 = document.getElementById('theEngine');
+let view2 = document.getElementById('theEngine');
 let engine = document.getElementById('engineWrapper');
 
-let open1 = function() {
+let open2 = function() {
     engine.style.display = 'block';
 };
 
@@ -24,5 +24,20 @@ let close = function() {
     engine.style.display = 'none';
 };
 
-view1.addEventListener('click', open1);
+view2.addEventListener('click', open2);
 engine.addEventListener('click', close);
+
+//
+let view3 = document.getElementById('Ada');
+let adaBox = document.getElementById('adaWrapper');
+
+let open3 = function() {
+    adaBox.style.display = 'block';
+};
+
+let close1 = function() {
+    adaBox.style.display = 'none';
+};
+
+view3.addEventListener('click', open3);
+adaBox.addEventListener('click', close1);
